05ec1f3be085f8789046a39bf9849ec45995876e
lohchanhin <chanhin17@gmail.com>
2022-02-26 23:17:41 +0800

    first

---
+++ app.js
@@ -0,0 +1,18 @@
+let buttonMenu = document.querySelector("div.menuButton");
+
+buttonMenu.addEventListener("click", e => {
+    let menu = document.querySelector("div.slideMenu");
+    if (menu.classList.value == "slideMenu") {
+       
+        menu.classList.toggle("trigger");
+        console.log(menu.classList.value);
+        menu.style.animation = "moveIn 1s ease forwards";
+        buttonMenu.classList.toggle("trigger");        
+    } else {
+        menu.classList.toggle("trigger");
+        console.log(menu.classList.value);
+        menu.style.animation = "moveOut 1s ease forwards";
+        buttonMenu.classList.toggle("trigger");
+    }
+    buttonMenu.ani
+})
