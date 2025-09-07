375b6099ec10e539a9eba2e39be17149bc4a9083
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-09-08 02:35:39 +0800

    調整 middleware，確保語系攔截涵蓋所有路徑

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import intlConfig from './next-intl.config';
 
 export default createMiddleware(intlConfig);
 export const config = {
-  matcher: ['/((?!_next|.*\\..*).*)'], // 覆蓋所有非靜態檔案路徑
+  matcher: ['/', '/(ms|en|zh-CN)/:path*']
 };
